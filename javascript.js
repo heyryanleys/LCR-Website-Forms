ae9b379fa97ddecdeadea3b8cacb9d1e596fa648
Ryan Leys <heyryanleys@gmail.com>
2017-11-28 09:33:17 -0500

    Including CSS and editing JS to be able to work with two forms on one page

--- javascript.js
+++ javascript.js
@@ -1,26 +1,24 @@
-<script>
-  function hsForms(){
+  function hsForms() {
     var hsEbook = document.getElementsByClassName('HS-eBook');
     var hsContactUs = document.getElementsByClassName('HS-ContactUs');
     var hsNewsletter = document.getElementsByClassName('HS-Newsletter');
     if (hsEbook.length > 0 && $('.hs-button').is(':visible')) {
-      $('.hs-button').attr({
+      $('.HS-eBook').find('.hs-button').attr({
         onclick: "dataLayer.push({ 'event' :  'trackEvent', 'eventCategory' : 'Ebook', 'eventAction' : 'Download', 'eventLabel' : '#CTA-btn ebook download now' })",
       });
       clearInterval(hsTimer)
     }
     if (hsContactUs.length > 0 && $('.hs-button').is(':visible')) {
-      $('.hs-button').attr({
+      $('.HS-ContactUs').find('.hs-button').attr({
         onclick: "dataLayer.push({ 'event' :  'trackEvent', 'eventCategory' : 'Contactform', 'eventAction' : 'Submit', 'eventLabel' : '#CTA-btn general contact' })",
       });
       clearInterval(hsTimer)
     }
     if (hsNewsletter.length > 0 && $('.hs-button').is(':visible')) {
-      $('.hs-button').attr({
+      $('.HS-Newsletter').find('.hs-button').attr({
         onclick: "dataLayer.push({ 'event' :  'trackEvent', 'eventCategory' : 'Newsletter', 'eventAction' : 'Submit', 'eventLabel' : '#Newslettersignon' })",
       });
       clearInterval(hsTimer)
     }
   }
   var hsTimer = window.setInterval(hsForms, 2000);
-</script>
